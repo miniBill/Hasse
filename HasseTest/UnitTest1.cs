a34ac7b79a30ee96396e4a881d01ac1a9bd70dcb
Leonardo Taglialegne <leonardo.taglialegne@gmail.com>
2012-12-10 21:04:09 +0100

    Adding tests

---
+++ HasseTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HasseTest {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
